d1f9fd18983066d47b58fb65c509c359be81289f
aylatshjhb024 <aylatshjhb024@student.wethinkcode.co.za>
2025-03-25 06:31:26 +0200

    Project is now in spring boot mode

---
+++ src/test/java/com/newtechcoder/NeoVault/NeoVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newtechcoder.NeoVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
